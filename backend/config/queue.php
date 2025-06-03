bb5fec473bdd954132a8a101275b1304407f4389
JesusKuro <jesuscanicio33@gmail.com>
2025-06-03 19:07:17 +0200

    chore: revert server IP addresses to localhost in configuration files
    - Updated configuration files to replace all instances of '92.112.194.87' with 'localhost' for local development consistency, including Docker, Apache, mail, queue, and Nginx settings.

--- backend/config/queue.php
+++ backend/config/queue.php
@@ -45,7 +45,7 @@ return [
 
         'beanstalkd' => [
             'driver' => 'beanstalkd',
-            'host' => env('BEANSTALKD_QUEUE_HOST', '92.112.194.87'),
+            'host' => env('BEANSTALKD_QUEUE_HOST', 'localhost'),
             'queue' => env('BEANSTALKD_QUEUE', 'default'),
             'retry_after' => (int) env('BEANSTALKD_QUEUE_RETRY_AFTER', 90),
             'block_for' => 0,
