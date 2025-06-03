0e6e9bd669300a894dafd3e280189602b59747e5
JesusKuro <jesuscanicio33@gmail.com>
2025-06-03 03:30:31 +0200

    chore: enhance Dockerfile and CORS configuration for improved functionality
    - Added creation of the laravel.log file and updated its permissions in the Dockerfile for better logging management.
    - Modified CORS configuration to include additional allowed origins and exposed headers for enhanced cross-origin resource sharing.

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -22,14 +22,16 @@ return [
         'http://92.112.194.87:8001',
         'http://92.112.194.87:3000',
         'http://localhost:3000',
-        'http://127.0.0.1:3000'
+        'http://localhost:8001',
+        'http://127.0.0.1:3000',
+        'http://127.0.0.1:8001'
     ],
 
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['*'],
 
     'max_age' => 0,
 
