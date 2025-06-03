53b51665aad3ee2be2ec26d30127b84356a9a816
JesusKuro <jesuscanicio33@gmail.com>
2025-06-03 03:37:37 +0200

    chore: update CORS configuration to include new API path
    - Added 'http://92.112.194.87:8001/api/*' as an allowed origin to enhance cross-origin resource sharing for API requests.

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -24,7 +24,8 @@ return [
         'http://localhost:3000',
         'http://localhost:8001',
         'http://127.0.0.1:3000',
-        'http://127.0.0.1:8001'
+        'http://127.0.0.1:8001',
+        'http://92.112.194.87:8001/api/*'
     ],
 
     'allowed_origins_patterns' => [],
